e4ddf61691ccbb144e8b7ad9c7547e9799156607
ArtemSykhetskiy <2210ams@gmail.com>
2022-06-26 15:16:49 +0300

    finished with setup configuration

---
+++ Config/configs.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'db'=> [
+        'host' => getenv('DB_HOST') ?? '',
+        'user' => getenv('DB_USER') ?? '',
+        'password'=>getenv('DB_PASSWORD') ?? '',
+        'name'=>getenv('DB_NAME') ?? ''
+    ]
+];
